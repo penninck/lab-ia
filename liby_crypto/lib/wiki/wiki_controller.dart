8bc8eb0ea28497065eb9aad5b728240395684ee2
Geandre Penninck Dos Santos <geandre@brq.com>
2025-07-01 17:05:42 -0300

    teste21

--- liby_crypto/lib/wiki/wiki_controller.dart
+++ liby_crypto/lib/wiki/wiki_controller.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'models/wiki_article.dart';
 
 class WikiController {
@@ -8,11 +9,16 @@ class WikiController {
       summary: 'Uma explicação simples sobre blockchain.',
       content: 'Blockchain é uma tecnologia de registro distribuído...',
     ),
-    // Adicione mais artigos aqui
+    WikiArticle(
+      id: '2',
+      title: 'História das Criptomoedas',
+      summary: 'Breve histórico sobre a evolução das criptomoedas.',
+      content: 'As criptomoedas começaram em 2009 com o Bitcoin...',
+    ),
   ];
 
   List<WikiArticle> get articles => List.unmodifiable(_articles);
 
   WikiArticle? getById(String id) =>
-      _articles.firstWhere((a) => a.id == id, orElse: () => null);
+      _articles.firstWhereOrNull((a) => a.id == id);
 }
