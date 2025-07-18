41ddd32414d8e4d8559ebbc1b736eb58af79761a
Geandre Penninck Dos Santos <geandre@brq.com>
2025-07-18 18:30:42 -0300

    MVP v1

--- liby/lib/pages/editor.dart
+++ liby/lib/pages/editor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:liby_crypto/app_exports.dart';
+import 'package:liby/app_exports.dart';
 
 class EditorPage extends StatefulWidget {
   const EditorPage({Key? key}) : super(key: key);
--- liby/lib/widgets/menu_drawer.dart
+++ liby/lib/widgets/menu_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:liby_crypto/app_exports.dart';
+import 'package:liby/app_exports.dart';
 
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key? key}) : super(key: key);
