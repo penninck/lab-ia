776f406324e19d1c109622e864ab33dc83b9173c
Geandré <geandre.penninck@gmail.com>
2025-07-25 19:55:48 -0300

    Delete liby - UI directory

--- liby - UI/lib/app_exports.dart
+++
@@ -1,7 +0,0 @@
-export 'app_routes.dart';
-export 'data/repository.dart';
-export 'models/article.dart';
-export 'pages/home.dart';
-export 'pages/editor.dart';
-export 'widgets/menu_drawer.dart';
-export 'pages/article_detail.dart';
