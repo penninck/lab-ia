85add94118396bb98a05b2c3c5030a86940d5ea6
Geandré <geandre.penninck@gmail.com>
2025-07-25 19:55:08 -0300

    Delete liby directory

--- liby/android/app/src/main/kotlin/com/exemplo/liby_crypto/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.exemplo.liby_crypto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- liby/lib/app_routes.dart
+++
@@ -1,4 +0,0 @@
-class AppRoutes {
-  static const String home = '/';
-  static const String editor = '/editor';
-}
